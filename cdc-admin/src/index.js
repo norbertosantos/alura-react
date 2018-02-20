0434d71ca870370ab74f12a40df2677f3f73a504
Norberto da Silva Santos Filho - DATAPREVRJ <norberto.filho@dataprev.gov.br>
2018-02-20 16:21:55 -0300

    Alteração do layou inicial da aplicação

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './css/pure-min.css';
-import './css/side-menu.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
