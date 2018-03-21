dda7d7f343aebf1da5a905fcf21c295d833819a6
Norberto Filho <norbertofilho@MacBook-Pro-de-Norberto.local>
2018-03-21 16:08:14 -0300

    Inclusão do input custumizado e do Botão Submit

---
+++ cdc-admin/src/componentes/BotaoSubmit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class BotaoSubmit extends Component {
+
+    render() {
+        return (
+            <div className="pure-control-group">                                  
+                <label></label> 
+                <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+              </div>            
+        );
+    }
+}
