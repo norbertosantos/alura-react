7c03e91f6b6f106fbc136e198fe492d3480adebb
Norberto da Silva Santos Filho - DATAPREVRJ <norberto.filho@dataprev.gov.br>
2018-04-04 16:04:37 -0300

    Alterações para as páginas com o React Router e rotas filhas

--- cdc-admin/src/Autor.js
+++ cdc-admin/src/Autor.js
@@ -101,34 +101,39 @@ class TabelaAutores extends Component {
 
 export default class AutorBox extends Component {
 
-  constructor() {
-    super();    
-    this.state = {lista : []};    
+    constructor() {
+      super();    
+      this.state = {lista : []};    
+    }
+  
+    componentDidMount(){  
+      $.ajax({
+          url:"http://localhost:8080/api/autores",
+          dataType: 'json',
+          success:function(resposta){    
+            this.setState({lista:resposta});
+          }.bind(this)
+        } 
+      );          
+  
+      PubSub.subscribe('atualiza-lista-autores',function(topico,novaLista){
+        this.setState({lista:novaLista});
+      }.bind(this));
+    }   
+  
+  
+    render(){
+      return (
+        <div>
+          <div className="header">
+            <h1>Cadastro de autores</h1>
+          </div>
+          <div className="content" id="content">                            
+            <FormularioAutor/>
+            <TabelaAutores lista={this.state.lista}/>        
+          </div>      
+  
+        </div>
+      );
+    }
   }
-
-  componentDidMount(){  
-    $.ajax({
-        url:"http://localhost:8080/api/autores",
-        dataType: 'json',
-        success:function(resposta){    
-          this.setState({lista:resposta});
-        }.bind(this)
-      } 
-    );          
-
-    PubSub.subscribe('atualiza-lista-autores',function(topico,novaLista){
-      this.setState({lista:novaLista});
-    }.bind(this));
-  }   
-
-
-  render(){
-    return (
-      <div>
-        <FormularioAutor/>
-        <TabelaAutores lista={this.state.lista}/>
-
-      </div>
-    );
-  }
-}
--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AutorBox from './Autor';
+import Home from './Home';
+import {Router,Route,browserHistory,IndexRoute} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  (<Router history={browserHistory}>
+      <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="/autor" component={AutorBox}/>
+          <Route path="/livro"/>
+      </Route>
+  </Router>),
+  document.getElementById('root')
+);
